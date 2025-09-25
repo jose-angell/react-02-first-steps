e9b60a88f5624d3488dffa56a31a502daa9e9374
jose Angel <josegallardo.cor@gmail.com>
2025-09-24 23:25:11 -0600

    feat: primer componente

---
+++ src/FirstStepsApp.tsx
@@ -0,0 +1,15 @@
+
+export function FirstStepsApp(){
+    return (
+        <>
+        <h1>Hola mundo</h1>
+    <p> Esto es un parrafo</p>
+    <button>Click me</button>
+    <div>
+      <h2>Hola desde React</h2>
+      <p>Esto es un parrafo dentro de un div</p>
+      <button>Click me</button>
+    </div>
+    </>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FirstStepsApp } from './FirstStepsApp'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola mundo</h1>
-  </StrictMode>,
+    <FirstStepsApp/>
+  </StrictMode>
 )
