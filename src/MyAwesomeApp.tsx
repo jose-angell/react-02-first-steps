df66ce88d3d526ae8e7ba5814dd458c34865a6d5
jose Angel <josegallardo.cor@gmail.com>
2025-09-25 19:31:05 -0600

    feat: impresion de variables y estilos css

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,32 @@
+import type { CSSProperties } from "react";
+
+const firstName = 'Jose angel';
+const lastName = 'Gallardo';
+const favoriteGames = ['Metal Gear', 'Death Stranding', 'Half Life'];
+const isActive = false;
+const address ={
+    street: 'Calle falsa',
+    number: 123,
+    country: 'USA'
+}  
+const myStyles: CSSProperties = {
+
+    backgroundColor: '#fafafa',
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 30
+}
 export function MyAwesomeApp(){
+
     return (
         <>
-            <h1>Jose angel</h1>
-            <h3>Gallardo</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+            <p>{favoriteGames.join(', ')}</p>
+            <p>{isActive ? 'Activo' : 'No Activo'}</p>
+            <p>{address.street}</p>
+            <p style={myStyles}
+            >{JSON.stringify(address)}</p>
         </>
     )
 }
