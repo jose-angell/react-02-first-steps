2275ef1f9ab95d1b560cce974d3469dd382bb8a7
jose Angel <josegallardo.cor@gmail.com>
2025-09-29 23:22:31 -0600

    feat: pruebas unitarias para itemConter y my awesome app

--- src/MyAwesomeApp.test.tsx
+++ src/MyAwesomeApp.test.tsx
@@ -1,10 +1,35 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { MyAwesomeApp } from "./MyAwesomeApp";
 import {render, screen} from '@testing-library/react'
 
 describe('MyAwesomeApp', () => {
     test('should render firstName and lastName', () => {
+        const {container} = render(<MyAwesomeApp/>)
+        //screen.debug()
+        const h1 = container.querySelector('h1');
+        expect(h1?.innerHTML).toContain("Jose angel")
+        const h3 = container.querySelector('h3');
+        expect(h3?.innerHTML).toContain("Gallardo")
+    })
+    // test('should render firstName and lastName - screen', () => {
+    //     render(<MyAwesomeApp/>)
+    //     screen.debug()
+    //     const h1 = screen.getByRole('heading', {
+    //         level: 1
+    //     });
+    //     expect(h1.innerHTML).toContain("Jose angel")
+    //     const h3 = screen.getByRole('heading', {
+    //         level: 3
+    //     });
+    //     expect(h3.innerHTML).toContain("Gallardo")
+    // })
+    test('should match snapshot', () => {
+        const {container} = render(<MyAwesomeApp/>)
+        expect(container).toMatchSnapshot()
+    })
+// se asegura de que el componente no cambie
+    test('should match snapshot', () => {
         render(<MyAwesomeApp/>)
-        screen.debug()
+        expect(screen.getByTestId('div-app')).toMatchSnapshot()
     })
 })
--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -19,14 +19,14 @@ const myStyles: CSSProperties = {
 export function MyAwesomeApp(){
 
     return (
-        <>
-            <h1>{firstName}</h1>
+        <div data-testid="div-app">
+            <h1 data-testid="first-name-little">{firstName}</h1>
             <h3>{lastName}</h3>
-            <p>{favoriteGames.join(', ')}</p>
-            <p>{isActive ? 'Activo' : 'No Activo'}</p>
+            <p className="mi-clase-favorita">{favoriteGames.join(', ')}</p>
+            <h1>{isActive ? 'Activo' : 'No Activo'}</h1>
             <p>{address.street}</p>
             <p style={myStyles}
             >{JSON.stringify(address)}</p>
-        </>
+        </div>
     )
 }
